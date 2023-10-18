ae1b1556a62d97790c94e7b6a771f31a5fd3528e
adiucas <adi@ucabd.com>
2023-10-18 16:56:37 +0600

    weffwf

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(
+  function () {
+    var includes = $('[data-include]')
+    $.each(includes, function () {
+      var file = 'includes/' + $(this).data('include') + '.html'
+      $(this).load(file)
+    })
+  }
+)
+
+$('.first-option').hide();
+
+$('.custom-file input').change(function (e) {
+  if (e.target.files.length) {
+    $(this).next('.custom-file-label').html(e.target.files[0].name);
+  }
+});
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
